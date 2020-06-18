400c9bd0dd8fd075869996871f6e6effa7f2dbaa
eforbes24 <44680407+eforbes24@users.noreply.github.com>
2020-06-18 13:00:42 -0400

    Bash Script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+pip install xarray
+pip install holoviews
+pip install dask
+python3 -m pip install "dask[array]" --upgrade
+pip install datashader
+pip install papermill
+pip install colorama --upgrade
+pip install IPython
+pip install scikit-video
+pip install scikit-image
+pip install opencv-python
+pip install natsort
+pip install medpy
+pip install pyfftw
+pip install scikit-learn
+pip install statsmodels
+pip install cvxpy
+pip install ipywidgets
+pip install ffmpeg
+pip install av
+pip install zarr
