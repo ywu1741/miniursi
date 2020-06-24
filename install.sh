c0eab82a4a7aeb380eca5153f32e67c55bb48c61
Eden Forbes <eforbes@vassar.edu>
2020-06-24 11:01:14 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,173 +1,41 @@
 #!/bin/bash
-
-python3 -m pip install ansiwrap==0.8.4
-python3 -m pip install asciitree==0.3.3
-python3 -m pip install async_generator==1.10
-python3 -m pip install attrs==19.3.0
-python3 -m pip install av
-python3 -m pip install backcall==0.1.0
-python3 -m pip install black==19.10b0
-python3 -m pip install bleach==3.1.5
-python3 -m pip install panel==0.8.0
-python3 -m pip install bokeh==1.4.0
-python3 -m pip install bottleneck==1.3.2
-python3 -m pip install brotlipy==0.7.0
-python3 -m pip install pycairo
-python3 -m pip install certifi==2020.4.5.1
-python3 -m pip install cffi==1.14.0
-python3 -m pip install cftime==1.1.3
-python3 -m pip install chardet==3.0.4
-python3 -m pip install click==7.1.2
-python3 -m pip install cloudpickle==1.4.1
-python3 -m pip install colorama==0.4.3
-python3 -m pip install colorcet==2.0.1
-python3 -m pip install cryptography==2.9.2
-python3 -m pip install curl
-python3 -m pip install cvxpy==1.0.27
-python3 -m pip install cycler==0.10.0
-python3 -m pip install cytoolz==0.10.1
-python3 -m pip install dask==2.11.0
-python3 -m pip install datashader==0.10.0
-python3 -m pip install datashape
-python3 -m pip install decorator==4.4.2
-python3 -m pip install defusedxml==0.6.0
-python3 -m pip install multiprocess==0.70.9
-python3 -m pip install dill==0.3.1.1
-python3 -m pip install distributed==2.11.0
-python3 -m pip install ecos==2.0.7.post1
-python3 -m pip install entrypoints==0.3
-python3 -m pip install fastcache==1.1.0
-python3 -m pip install fasteners==0.14.1
-python3 -m pip install ffmpeg
-python3 -m pip install ffmpeg-python==0.2.0
-python3 -m pip install pyFFTW
-python3 -m pip install freetype-py
-python3 -m pip install fsspec==0.7.4
-python3 -m pip install future==0.18.2
-python3 -m pip install python-gettext
-python3 -m pip install PyGObject
-python3 -m pip install h4py
-python3 -m pip install h5py
-python3 -m pip install heapdict==1.0.1
-python3 -m pip install holoviews==1.12.7
-python3 -m pip install icc_rt==2019.0.0
-python3 -m pip install PyICU
-python3 -m pip install idna==2.9
-python3 -m pip install imagecodecs-lite==2019.12.3
-python3 -m pip install imageio==2.8.0
-python3 -m pip install importlib-metadata==1.6.0
-python3 -m pip install ipykernel==5.3.0
-python3 -m pip install ipython==7.12.0
-python3 -m pip install ipython_genutils==0.2.0
-python3 -m pip install ipywidgets==7.5.1
-python3 -m pip install jedi==0.17.0
-python3 -m pip install jinja2==2.11.2
-python3 -m pip install joblib==0.15.1
-python3 -m pip install jsonschema==3.2.0
-python3 -m pip install kiwisolver==1.2.0
-python3 -m pip install llvmlite==0.31.0
-python3 -m pip install locket==0.2.0
-python3 -m pip install markdown==3.2.2
-python3 -m pip install markupsafe==1.1.1
-python3 -m pip install matplotlib==3.1.3
-python3 -m pip install medpy==0.4.0
-python3 -m pip install mistune==0.8.4
-python3 -m pip install monotonic==1.5
-python3 -m pip install msgpack-python
-python3 -m pip install multipledispatch==0.6.0
-python3 -m pip install mypy_extensions==0.4.3
-python3 -m pip install natsort==7.0.1
-python3 -m pip install nbclient==0.3.1
-python3 -m pip install nbconvert==5.6.1
-python3 -m pip install nbformat==5.0.6
-python3 -m pip install nest-asyncio==1.3.3
-python3 -m pip install netcdf4==1.5.3
-python3 -m pip install networkx==2.4
-python3 -m pip install nodejs
-python3 -m pip install notebook==6.0.3
-python3 -m pip install numba==0.48.0
-python3 -m pip install numcodecs==0.6.4
-python3 -m pip install numpy==1.18.1
-python3 -m pip install olefile==0.46
-python3 -m pip install opencv-python
-python3 -m pip install openssl-python
-python3 -m pip install osqp==0.6.1
-python3 -m pip install packaging==20.4
-python3 -m pip install pandas==1.0.1
-python3 -m pip install pandocfilters==1.4.2
-python3 -m pip install papermill==2.0.0
-python3 -m pip install param==1.9.3
-python3 -m pip install parso==0.7.0
-python3 -m pip install partd==1.1.0
-python3 -m pip install pathspec==0.8.0
-python3 -m pip install patsy==0.5.1
-python3 -m pip install python-pcre
-python3 -m pip install pickleshare==0.7.5
-python3 -m pip install pillow==7.1.2
-python3 -m pip install prometheus_client==0.8.0
-python3 -m pip install prompt_toolkit==3.0.5
-python3 -m pip install psutil==5.7.0
-python3 -m pip install pycparser==2.20
-python3 -m pip install pyct==0.4.6
-python3 -m pip install pyfftw==0.12.0
-python3 -m pip install pygments==2.6.1
-python3 -m pip install pyopenssl==19.1.0
-python3 -m pip install pyparsing==2.4.7
-python3 -m pip install pyqt5
-python3 -m pip install pyqt5-sip==12.8
-python3 -m pip install pyqtchart==5.12
-python3 -m pip install pyqtwebengine==5.12.1
-python3 -m pip install pyreadline==2.1
-python3 -m pip install pyrsistent==0.16.0
-python3 -m pip install pysocks==1.7.1
-python3 -m pip install pytz==2020.1
-python3 -m pip install pyviz_comms==0.7.4
-python3 -m pip install pywavelets==1.1.1
-python3 -m pip install pywinpty==0.5.7
-python3 -m pip install pyyaml==5.3.1
-python3 -m pip install pyzmq==19.0.1
-python3 -m pip install pyqt
-python3 -m pip install qtconsole==4.7.4
-python3 -m pip install qtpy==1.9.0
-python3 -m pip install regex==2020.5.14
-python3 -m pip install requests==2.23.0
-python3 -m pip install scikit-image==0.16.2
-python3 -m pip install scikit-learn==0.22.1
-python3 -m pip install scipy==1.4.1
-python3 -m pip install scs
-python3 -m pip install send2trash==1.5.0
-python3 -m pip install setuptools==47.1.1
-python3 -m pip install simpleitk==1.2.4
-python3 -m pip install six==1.15.0
-python3 -m pip install sk-video==1.1.10
-python3 -m pip install sortedcontainers==2.1.0
-python3 -m pip install statsmodels==0.11.1
-python3 -m pip install tblib==1.6.0
-python3 -m pip install tenacity==6.2.0
-python3 -m pip install terminado==0.8.3
-python3 -m pip install testpath==0.4.4
-python3 -m pip install textwrap3==0.9.2
-python3 -m pip install tifffile==2020.2.16
-python3 -m pip install tk
-python3 -m pip install toml==0.10.1
-python3 -m pip install toolz==0.10.0
-python3 -m pip install tornado==6.0.4
-python3 -m pip install tqdm==4.43.0
-python3 -m pip install traitlets==4.3.3
-python3 -m pip install typed-ast==1.4.1
-python3 -m pip install typing_extensions==3.7.4.2
-python3 -m pip install urllib3==1.25.9
-python3 -m pip install vc
-python3 -m pip install wcwidth==0.2.3
-python3 -m pip install webencodings==0.5.1
-python3 -m pip install wheel==0.34.2
-python3 -m pip install widgetsnbextension==3.5.1
-python3 -m pip install win_inet_pton==1.1.0
-python3 -m pip install wincertstore==0.2
-python3 -m pip install pywinpty
-python3 -m pip install xarray==0.15.0
-python3 -m pip install zarr==2.4.0
-python3 -m pip install pyzmq
-python3 -m pip install zict==2.0.0
-python3 -m pip install zipp==3.1.0
-python3 -m pip install zstd==1.4.4
+pip install --user av
+pip install --user panel==0.8.0
+pip install --user bokeh==1.4.0
+pip install --user bottleneck==1.3.2
+pip install --user pycairo
+pip install --user cvxpy==1.0.27
+pip install --user dask==2.11.0
+pip install --user datashader==0.10.0
+pip install --user distributed==2.11.0
+pip install --user ecos==2.0.7.post1
+pip install --user ffmpeg
+pip install --user ffmpeg-python==0.2.0
+pip install --user pyFFTW
+pip install --user holoviews==1.12.7
+pip install --user ipython==7.12.0
+pip install --user ipython_genutils==0.2.0
+pip install --user matplotlib==3.1.3
+pip install --user medpy==0.4.0
+pip install --user natsort==7.0.1
+pip install --user netcdf4==1.5.3
+pip install --user networkx==2.4
+pip install --user nodejs
+pip install --user numba==0.48.0
+pip install --user numpy==1.18.1
+pip install --user opencv-python
+pip install --user pandas==1.0.1
+pip install --user papermill==2.0.0
+pip install --user param==1.9.3
+pip install --user pyfftw==0.12.0
+pip install --user scikit-image==0.16.2
+pip install --user scikit-learn==0.22.1
+pip install --user scipy==1.4.1
+pip install --user scs
+pip install --user simpleitk==1.2.4
+pip install --user sk-video==1.1.10
+pip install --user statsmodels==0.11.1
+pip install --user tifffile==2020.2.16
+pip install --user tqdm==4.43.0
+pip install --user xarray==0.15.0
+pip install --user zarr==2.4.0
