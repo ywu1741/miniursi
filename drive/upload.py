6b0081853fdf49547f3342995ceced8a11897976
eforbes24 <44680407+eforbes24@users.noreply.github.com>
2020-06-25 17:53:37 -0400

    Update upload.py

--- drive/upload.py
+++ drive/upload.py
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-from drive_list.py import SCOPES, store, creds, DRIVE, search, upload
+from drive_list import SCOPES, store, creds, DRIVE, search, upload
 
 upload('1tr_Laq--VNw5gyVPYQgASNY3iFp9aYyX')
