df20b79fe356af19582d32bc7fe6cedf7d394ebf
eforbes24 <44680407+eforbes24@users.noreply.github.com>
2020-07-16 15:00:59 -0400

    Update upload.py

--- drive/upload.py
+++ drive/upload.py
@@ -2,4 +2,4 @@
 
 from drive_list import SCOPES, store, creds, DRIVE, search, upload
 
-upload('1G8-4rBwtXhSxcuRAEkQ56QJlWWmybYRl')
+upload('1G8-4rBwtXhSxcuRAEkQ56QJlWWmybYRl', '/work/miniscopepipeline/miniursi/videos/Mouse_16/Day_2_FC/')
