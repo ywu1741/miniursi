5135095ff2fe43982217ef3cc698374c52b44d5c
eforbes24 <44680407+eforbes24@users.noreply.github.com>
2020-07-20 14:36:28 -0400

    Update ursi_crossreg.py

--- ursi_crossreg.py
+++ ursi_crossreg.py
@@ -2,7 +2,9 @@
 # coding: utf-8
 
 # # load config
-from minian.config_crossreg import minian_path, dpath, f_pattern, id_dims, param_t_dist, output_size
+dpath = "./videos/ENTER/PATH/HERE/
+
+from minian.config_crossreg import minian_path, f_pattern, id_dims, param_t_dist, output_size
 
 # # Load Modules
 
