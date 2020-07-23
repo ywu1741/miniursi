dbb5daf49f228d6dbaada8ca08317a45091335c3
eforbes24 <44680407+eforbes24@users.noreply.github.com>
2020-07-23 11:51:14 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+pip install --user av
+pip install --user panel==0.8.0
+pip install --user bokeh==1.4.0
+pip install --user bottleneck==1.3.2
+pip install --user cycler
+# pip install --user Pillow==5.3.0
+# pip install --user idna==2.6
+# pip install --user requests==2.18.4
+# pip install --user urllib3==1.22
+# pip install --user pycairo
+pip install --user cvxpy==1.0.27
+pip install --user dask==2.11.0
+pip install --user datashader==0.10.0
+pip install --user distributed==2.11.0
+pip install --user ecos==2.0.7.post1
+pip install --user ffmpeg
+pip install --user ffmpeg-python==0.2.0
+pip install --user pyFFTW
+pip install --user holoviews==1.12.7
+pip install --user ipython==7.12.0
+pip install --user ipython_genutils==0.2.0
+pip install --user ipywidgets
+pip install --user matplotlib==3.1.3
+pip install --user medpy==0.4.0
+pip install --user natsort==7.0.1
+pip install --user netcdf4==1.5.3
+pip install --user networkx==2.4
+pip install --user nodejs
+pip install --user numba==0.48.0
+pip install --user numpy==1.18.1
+pip install --user opencv-python
+pip install --user pandas==1.0.1
+pip install --user papermill==2.0.0
+pip install --user param==1.9.3
+pip install --user psutil
+pip install --user pyparsing
+pip install --user pyfftw==0.12.0
+pip install --user scikit-image==0.16.2
+pip install --user scikit-learn==0.22.1
+pip install --user scipy==1.4.1
+pip install --user scs
+pip install --user setuptools==47.1.1
+pip install --user simpleitk==1.2.4
+pip install --user sk-video==1.1.10
+pip install --user statsmodels==0.11.1
+pip install --user tifffile==2020.2.16
+pip install --user tqdm==4.43.0
+pip install --user xarray==0.15.0
+pip install --user zarr==2.4.0
+pip install --user gdown
+pip install --user google-api-python-client
+pip install --user oauth2client
+
+# pip install --user deeplabcut
+# pip install --user \
+#     -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-16.04 \
+#     wxPython
+# pip install --user wxPython
+
+pip install --user tensorflow
